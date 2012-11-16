cde212c2b9ac0ddcebe96976f17fc8ae52db5239
Michael Filbin <michael.filbin@gmail.com>
2012-11-15 19:45:33 -0700

    Integrate moment.js to do dynamic date ranges on upcoming events service
    Signed-off-by: Michael Filbin <michael.filbin@gmail.com>

--- proxies/PersonalHomeAPI.js
+++ proxies/PersonalHomeAPI.js
@@ -1,6 +1,8 @@
 var path = require('path');
 var nconf = require('nconf');
 var restify = require('restify');
+var moment = require('moment');
+
 var Error = require(path.join(__dirname, '..', 'errors'));
 var helpers = require(path.join(__dirname, '..', 'helpers'));
 
@@ -23,9 +25,11 @@ PersonalHomeAPI.prototype.getExams = function getExams(authToken, callback) {
 };
 
 PersonalHomeAPI.prototype.getEvents = function getEvents(authToken, callback) {
+  var today = moment(new Date());
+  var dateQueryString = "?from="+(today.format('M/D/YYYY'))+"&until="+(today.add("days", 7).format('M/D/YYYY'));
   if (!authToken) return callback(new Error.InvalidCredentialsError("Missing authentication token"));
   var client = helpers.createJSONClient(nconf.get("PERSONAL_HOME_HOST"), {"x-authorization": authToken});
-  return client.get(this.eventsService, helpers.handleResponse.bind(this, callback));
+  return client.get((this.eventsService + dateQueryString), helpers.handleResponse.bind(this, callback));
 };
 
 
